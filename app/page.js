dce0d46bba568c3c8dad9c34ba1ab95995624e85
gtscottyyy <gtscottyyy@gmail.com>
2024-01-18 19:30:24 -0500

    project init and functionality

--- app/page.js
+++ app/page.js
@@ -1,95 +1,132 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
 
-export default function Home() {
+import { Provider } from "@self.id/react";
+import { Web3Provider } from "@ethersproject/providers";
+import { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+import { useViewerConnection } from "@self.id/react";
+import { EthereumAuthProvider } from "@self.id/web";
+import styles from "../app/page.module.css";
+import { useViewerRecord } from "@self.id/react";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+function MyApp({ Component, pageProps }) {
+  const web3ModalRef = useRef();
+  const queryClient = new QueryClient();
+
+  const getProvider = async () => {
+    const provider = await web3ModalRef.current.connect();
+    const wrappedProvider = new Web3Provider(provider);
+    return wrappedProvider;
+  };
+
+  const [connection, connect, disconnect] = useViewerConnection();
+
+  const connectToSelfID = async () => {
+    const ethereumAuthProvider = await getEthereumAuthProvider();
+    connect(ethereumAuthProvider);
+  };
+
+  const getEthereumAuthProvider = async () => {
+    const wrappedProvider = await getProvider();
+    const signer = wrappedProvider.getSigner();
+    const address = await signer.getAddress();
+    return new EthereumAuthProvider(wrappedProvider.provider, address);
+  };
+
+  useEffect(() => {
+    if (connection.status !== "connected") {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+    }
+  }, [connection.status]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <QueryClientProvider client={queryClient}>
+      <div className={styles.main}>
+        <div className={styles.navbar}>
+          <span className={styles.title}>Ceramic Demo</span>
+          {connection.status === "connected" ? (
+            <span className={styles.subtitle}>Connected</span>
+          ) : (
+            <button
+              onClick={connectToSelfID}
+              className={styles.button}
+              disabled={connection.status === "connecting"}
+            >
+              Connect
+            </button>
+          )}
+        </div>
+
+        <div className={styles.content}>
+          <div className={styles.connection}>
+            {connection.status === "connected" ? (
+              <div>
+                <span className={styles.subtitle}>
+                  Your 3ID is {connection.selfID.id}
+                </span>
+                <RecordSetter />
+              </div>
+            ) : (
+              <span className={styles.subtitle}>
+                Connect with your wallet to access your 3ID
+              </span>
+            )}
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </QueryClientProvider>
   );
 }
+
+function RecordSetter() {
+  const record = useViewerRecord("basicProfile");
+  const [name, setName] = useState("");
+
+  const updateRecordName = async (name) => {
+    await record.merge({
+      name: name,
+    });
+  };
+
+  return (
+    <div className={styles.content}>
+      <div className={styles.mt2}>
+        {record.content ? (
+          <div className={styles.flexCol}>
+            <span className={styles.subtitle}>
+              Hello {record.content.name}!
+            </span>
+
+            <span>
+              The above name was loaded from Ceramic Network. Try updating it
+              below.
+            </span>
+          </div>
+        ) : (
+          <span>
+            You do not have a profile record attached to your 3ID. Create a
+            basic profile by setting a name below.
+          </span>
+        )}
+      </div>
+
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles.mt2}
+      />
+      <button className={styles.button} onClick={() => updateRecordName(name)}>
+        Update
+      </button>
+    </div>
+  );
+}
+
+export default MyApp;
